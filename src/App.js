8f285a6a8fd10fdcdb67840d089845a8ba1f1ad8
8Firdavs8 <atabfi01@luther.edu>
2021-12-07 10:50:12 -0600

    project form for user input

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import {Route, Routes} from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewFavouritesPage from './pages/Favourites';
 import NewMeetupsPage from './pages/NewMeetup';
-import MainNavigation from './components/layout/MainNavigation'
+// import MainNavigation from './components/layout/MainNavigation'
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+
+    <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='book-list' element = {<NewMeetupsPage />} />
         <Route path='books-stored' element={<NewFavouritesPage />} />
       </Routes>
-    </div>
+    </Layout>
+    
+    
+    
   );
 }
 
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,22 +1,36 @@
 import MeetupList from '../components/meetups/MeetupList';
+import Wrapper from '../components/ui/Wrapper';
 
 
 
 
 function AllMeetupsPage() {
     return (
-      <form>
-        <label>
-          Book Name:
-        </label>
-        <div>
-          <input type = "text"></input>
-          <buton>Search</buton>
+      <Wrapper>
+        <form className = 'form'>
+          <div>
+            <p>Book Title: </p>
+            <input type = "text"></input>
+          </div>
+          <div>
+            <p>ISBN Number: </p>
+            <input type = "text"></input>
+          </div>
+          <div>
+            <p>Author: </p>
+            <input type = "text"></input>
+          </div>
+          <br></br>
+          <div>
+            <button>Search</button>
+          </div>
+          
+          
 
-        </div>
-        
+        </form>
 
-      </form>
+      </Wrapper>
+      
         
     )
 
