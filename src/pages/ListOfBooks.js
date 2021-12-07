5b6cc69c16415092c3ea94d3f1651708afe1b7a6
isaac-list <listis01@luther.edu>
2021-12-07 16:09:31 -0600

    access a Nodejs backend on a local machine

--- src/pages/ListOfBooks.js
+++ src/pages/ListOfBooks.js
@@ -1,7 +1,19 @@
+import React from "react";
+
 function ListOfBooksPage() {
+	const [data, setData] = React.useState(null);
+	
+	  React.useEffect(() => {
+	      fetch("http://127.0.0.1:3000/api/books")
+	        .then((res) => res.json())
+	        .then((data) => setData(data));
+	  }, []);
+	  
     return (
         <div>
             The list of Books to Search
+
+            <p>{!data ? "Loading..." : data}</p>
         </div>
     )
 
