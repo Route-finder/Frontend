3c039e671cc6857e1456dc6287a76ef0501c7718
8Firdavs8 <atabfi01@luther.edu>
2021-12-07 14:30:39 -0600

    user input management

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,55 @@
 import MeetupList from '../components/meetups/MeetupList';
 import Wrapper from '../components/ui/Wrapper';
+import {useState} from 'react';
 
 
 
 
 function AllMeetupsPage() {
+
+  const [text, setText] = useState(null);
+   
+    const clickHandler = (event) => {
+        // When clicked, set text to value of input box
+        event.preventDefault();
+        // console.log("Clicked");
+        let s = document.getElementById("search");
+        let b = document.getElementById("search1");
+        let c = document.getElementById("search2");
+        let allText = "Book Name: " + s.value + ", " + " ISBN: " + b.value + ", " + " Author: " + c.value;
+
+        setText(allText);
+        
+    };
+
+
+
+
+
     return (
       <Wrapper>
         <form className = 'form'>
           <div>
             <p>Book Title: </p>
-            <input type = "text"></input>
+            <input id="search" type = "text"></input>
           </div>
           <div>
             <p>ISBN Number: </p>
-            <input type = "text"></input>
+            <input id="search1" type = "text"></input>
           </div>
           <div>
             <p>Author: </p>
-            <input type = "text"></input>
+            <input id="search2" type = "text"></input>
           </div>
           <br></br>
           <div>
-            <button>Search</button>
+            <button onClick = {clickHandler}>Search</button>
           </div>
         </form>
+
+        <p>
+          {!text ? "Empty Basket, please choose a book": text}
+        </p>
 
       </Wrapper>
       
