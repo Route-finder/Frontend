eb0792f65b575c45ce0501978fc10baf5d476473
isaac-list <listis01@luther.edu>
2021-12-07 20:44:35 -0600

    update api url

--- src/pages/ListOfBooks.js
+++ src/pages/ListOfBooks.js
@@ -3,20 +3,21 @@ import React from "react";
 function ListOfBooksPage() {
 	const [data, setData] = React.useState(null);
 	
-	  React.useEffect(() => {
-	      fetch("http://127.0.0.1:3000/api/books")
-	        .then((res) => res.json())
-	        .then((data) => setData(data));
-	  }, []);
+		React.useEffect(() => {
+			fetch("http://library-guide.herokuapp.com/api/books")
+				.then((res) => res.json())
+				.then((data) => {
+					setData(data)
+				});
+		}, []);
 	  
     return (
         <div>
             The list of Books to Search
 
-            <p>{!data ? "Loading..." : data}</p>
+            <p>{!data ? "Loading..." : "Data coming soon :D"}</p>
         </div>
     )
-
 }
 
 export default ListOfBooksPage;
