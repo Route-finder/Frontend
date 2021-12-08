dc926be468378bf889532223fffca1dc55d21eec
isaac-list <listis01@luther.edu>
2021-12-07 21:27:29 -0600

    unsuccessful attempt to implement react-table

--- src/pages/ListOfBooks.js
+++ src/pages/ListOfBooks.js
@@ -5,25 +5,90 @@
  */
 
 import React from "react";
+import ReactTable from "react-table";
 
 function ListOfBooksPage() {
 	const [data, setData] = React.useState(null);
 	
-		React.useEffect(() => {
-			fetch("http://library-guide.herokuapp.com/api/books")
-				.then((res) => res.json())
-				.then((data) => {
-					setData(data)
-				});
-		}, []);
-	  
+	/**
+	 * Data in form:
+	 * {results: [
+	 *   {isbn, author, title, call_no},
+	 *   ...
+	 * ]}
+	 */
+	React.useEffect(() => {
+		fetch("http://library-guide.herokuapp.com/api/books")
+			.then((res) => res.json())
+			.then((data) => {
+				setData(data.results)
+			});
+	}, []);
+
+	const tableData = React.useMemo(() => [
+		{ col1: 'Hello', col2: 'World'},
+		{col1: 'react-table', col2: 'rocks'},
+		{col1: 'whatever', col2: 'you want'}
+	], []);
+
+	const columns = React.useMemo(() => [
+			{ Header: 'Title', accessor: 'title' },
+			{ Header: 'Author', accessor: 'author' },
+			{ Header: 'Call Number', accessor: 'call_no' }
+		],
+		[]
+	);
+
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		rows,
+		prepareRow,
+	} = ReactTable.useTable({columns, tableData})
+
     return (
         <div>
-            The list of Books to Search
+            <h1>Books to be Found</h1>
 
-            <p>{!data ? "Loading..." : "Data coming soon :D"}</p>
+			<table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+       		<thead>
+				{headerGroups.map(headerGroup => (
+					<tr {...headerGroup.getHeaderGroupProps()}>
+						{headerGroup.headers.map(column => (
+							<th {...column.getHeaderProps()} style={{
+								borderBottom: 'solid 3px red',
+								background: 'aliceblue',
+								color: 'black',
+								fontWeight: 'bold',
+							}}>
+								{column.render('Header')}
+							</th>
+						))}
+					</tr>
+				))}
+			</thead>
+       		<tbody {...getTableBodyProps()}>
+				{rows.map(row => {
+				prepareRow(row)
+				return (
+					<tr {...row.getRowProps()}>
+						{row.cells.map(cell => {
+						return (
+							<td {...cell.getCellProps()} style={{
+								padding: '10px',
+								border: 'solid 1px gray',
+								background: 'papayawhip',
+							}}>
+								{cell.render('Cell')}
+							</td>
+						)})}
+					</tr>
+				)})}
+			</tbody>
+		</table>
         </div>
-    )
+    );
 }
 
 export default ListOfBooksPage;
