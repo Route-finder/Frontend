76f1718357ac8ddfb3802e4818e014aa22e31bb8
isaac-list <listis01@luther.edu>
2021-12-12 19:36:37 -0600

    can now add books via ISBN from home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,9 +8,6 @@ import MeetupList from '../components/meetups/MeetupList';
 import Wrapper from '../components/ui/Wrapper';
 import {useState} from 'react';
 
-
-
-
 function HomePage() {
 
   const [text, setText] = useState(null);
@@ -19,52 +16,51 @@ function HomePage() {
         // When clicked, set text to value of input box
         event.preventDefault();
         // console.log("Clicked");
-        let author = document.getElementById("search");
+        let title = document.getElementById("search");
         let isbn = document.getElementById("search1");
-        let title = document.getElementById("search2");
+        let author = document.getElementById("search2");
 
-		/**
-		 * Submit request to backend with info for OCLC request
-		 */
+      /**
+       * Submit request to backend with info for OCLC request
+       */
 
-		// If ISBN provided, select the first result
-		if (isbn.value != "") {
-			let response = fetch("https://library-guide.herokuapp.com/api/search", {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json;charset=utf-8'
-				},
-				body: JSON.stringify(isbn.value)
-			});
-      		console.log(response);
-		}
+      // If ISBN provided, select the first result
+      if (isbn.value != "") {
+        const requestOptions = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify({isbn: isbn.value})
+        };
 
-		// If title or author provided, send as a request
-		else if (author.vaule != "" || title.value != "") {
-			let response = fetch("https://library-guide.herokuapp.com/api/search", {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json;charset=utf-8'
-				},
-				body: JSON.stringify({author: author.vaule, title: title.value})
-			});
-		}
+        fetch("https://library-guide.herokuapp.com/api/search", requestOptions)
+          .then(response => response.json())
+          .then(data => console.log(data));
+      }
 
-		// Otherwise, return an error if no values provided
-		else {
-			alert("Please enter one or more values");
-		}
-		
-        
-        let allText = "Book Name: " + s.value + ", " + " ISBN: " + b.value + ", " + " Author: " + c.value;
+      // If title or author provided, send as a request
+      else if (author.vaule != "" || title.value != "") {
+        let response = fetch("https://library-guide.herokuapp.com/api/search", {
+        // let response = fetch("http://localhost:3100/api/search/", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify({author: author.vaule, title: title.value})
+        });
+        console.log(response);
+      }
 
-        setText(allText);
-        
+      // Otherwise, return an error if no values provided
+      else {
+        alert("Please enter one or more values");
+      }
+      
+      let allText = "Book Name: " + title.value + ", " + " ISBN: " + isbn.value + ", " + " Author: " + author.value;
+
+      setText(allText);
     };
-
-
-
-
 
     return (
       <Wrapper>
